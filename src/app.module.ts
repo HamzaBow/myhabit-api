2fea922f813075c2a35320c4305877af95e34151
Hamza Bow <hamzabow77@gmail.com>
2022-06-24 10:43:09 +0100

    add habitgroup module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { HabitGroupModule } from './habit-group/habit-group.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, HabitGroupModule],
 })
 export class AppModule {}
