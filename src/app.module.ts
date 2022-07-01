dd6e0fde42596ad31ec964fd2d139ba00bf30aea
Hamza Bow <hamzabow77@gmail.com>
2022-07-01 18:06:49 +0100

    create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { HabitGroupModule } from './habit-group/habit-group.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, HabitGroupModule, PrismaModule],
+  imports: [UserModule, HabitGroupModule, PrismaModule, AuthModule],
 })
 export class AppModule {}
