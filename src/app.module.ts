de89c24aea26dc59a940e5ed1b81d014b65932a4
Hamza Bow <hamzabow77@gmail.com>
2022-07-01 15:12:45 +0100

    create prisma module (nest module - in src)
    as opposed to the one in the top level folder
    which contains prisma models and migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { HabitGroupModule } from './habit-group/habit-group.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, HabitGroupModule],
+  imports: [UserModule, HabitGroupModule, PrismaModule],
 })
 export class AppModule {}
